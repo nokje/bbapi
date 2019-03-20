0384090c819d7c1e2d51d84f04a175f4d8e5480f
nokje <reda-@live.nl>
2019-03-20 13:26:53 +0100

    reply van de API!

--- apicall.sh
+++ apicall.sh
@@ -6,6 +6,5 @@
 $authkey = cat ./apikey
 $apicat = https://api.sandbox.bigbuy.eu/rest/catalog/categories.json
 
-curl -v --header "WWW-Authenticate: NTg3ZDQ1NTZkNjQ4M2RiMDk2MGY0ODNkMzhmYzM0ZDNlZDdmNTBhN2Y2YjIzNGM5MjEzOGViMzg1Nzc1MjhlZA" https://api.sandbox.bigbuy.eu/rest/catalog/categories.json
-#curl -v --header "auth: $authkey" $apicat
-#echo ($authkey)
+#curl -v --header "Authorization: Bearer NTg3ZDQ1NTZkNjQ4M2RiMDk2MGY0ODNkMzhmYzM0ZDNlZDdmNTBhN2Y2YjIzNGM5MjEzOGViMzg1Nzc1MjhlZA" https://api.sandbox.bigbuy.eu/rest/catalog/categories.json
+curl --header "Authorization: Bearer NTg3ZDQ1NTZkNjQ4M2RiMDk2MGY0ODNkMzhmYzM0ZDNlZDdmNTBhN2Y2YjIzNGM5MjEzOGViMzg1Nzc1MjhlZA" https://api.sandbox.bigbuy.eu/rest/catalog/categories.json > output && cat output | jq > jsonformat
