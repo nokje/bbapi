dcf7b2c0a6d22e37b758676201dfa7b9d2dabbba
nokje <reda-@live.nl>
2019-03-20 15:23:30 +0100

    function van gemaakt :+1:

--- apicall.sh
+++ apicall.sh
@@ -3,8 +3,26 @@
 #documentatie van de API is te vinden bij http://api.bigbuy.eu/doc
 
 #VARs
-$authkey = cat ./apikey
-$apicat = https://api.sandbox.bigbuy.eu/rest/catalog/categories.json
+key=NTg3ZDQ1NTZkNjQ4M2RiMDk2MGY0ODNkMzhmYzM0ZDNlZDdmNTBhN2Y2YjIzNGM5MjEzOGViMzg1Nzc1MjhlZA
 
-#curl -v --header "Authorization: Bearer NTg3ZDQ1NTZkNjQ4M2RiMDk2MGY0ODNkMzhmYzM0ZDNlZDdmNTBhN2Y2YjIzNGM5MjEzOGViMzg1Nzc1MjhlZA" https://api.sandbox.bigbuy.eu/rest/catalog/categories.json
-curl --header "Authorization: Bearer NTg3ZDQ1NTZkNjQ4M2RiMDk2MGY0ODNkMzhmYzM0ZDNlZDdmNTBhN2Y2YjIzNGM5MjEzOGViMzg1Nzc1MjhlZA" https://api.sandbox.bigbuy.eu/rest/catalog/categories.json > output && cat output | jq > jsonformat
+
+#"\"/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl\""
+getcat() {
+  curl -H "Authorization: Bearer $key" https://api.sandbox.bigbuy.eu/rest/catalog/categories.json?isoCode=en > output.raw
+}
+
+getcat
+#"\ -H\ "Authorization: Bearer NTg3ZDQ1NTZkNjQ4M2RiMDk2MGY0ODNkMzhmYzM0ZDNlZDdmNTBhN2Y2YjIzNGM5MjEzOGViMzg1Nzc1MjhlZA"
+
+#Example CURL
+#curl --header "Authorization: Bearer NTg3ZDQ1NTZkNjQ4M2RiMDk2MGY0ODNkMzhmYzM0ZDNlZDdmNTBhN2Y2YjIzNGM5MjEzOGViMzg1Nzc1MjhlZA" https://api.sandbox.bigbuy.eu/rest/catalog/categories.json?isoCode=en > output.raw
+#curl -H "Authorization: Bearer NTg3ZDQ1NTZkNjQ4M2RiMDk2MGY0ODNkMzhmYzM0ZDNlZDdmNTBhN2Y2YjIzNGM5MjEzOGViMzg1Nzc1MjhlZA" https://api.sandbox.bigbuy.eu/rest/catalog/categories.json?isoCode=en > output.raw
+#curlkey https://api.sandbox.bigbuy.eu/rest/catalog/categories.json?isoCode=en > output.raw
+#echo "curl -v $key https://api.sandbox.bigbuy.eu/rest/catalog/categories.json?isoCode=en > output.raw"
+#echo "curl $key https://api.sandbox.bigbuy.eu/rest/catalog/categories.json?isoCode=en > output.raw"
+
+#Verwerk de output data van raw data naar human readable jsonformat
+#cat output.raw | jq > output.json
+#curl -H "Accept: application/xhtml+xml"  -H "Authorization: Bearer NTg3ZDQ1NTZkNjQ4M2RiMDk2MGY0ODNkMzhmYzM0ZDNlZDdmNTBhN2Y2YjIzNGM5MjEzOGViMzg1Nzc1MjhlZA" https://api.sandbox.bigbuy.eu/rest/catalog/categories.json?isoCode=en > output.raw
+
+#curl $
