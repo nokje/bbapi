16326ea1b34737bae0a0d8d625f3eea452458d74
nokje <reda-@live.nl>
2019-03-21 14:07:15 +0100

    ben nu wel klaar, beetje test vriendelijk gemaakt met een cleanup er in

--- bbapi.sh
+++ bbapi.sh
@@ -15,6 +15,28 @@ json_img=images.json
 key='cat apikey'
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 
+#clean_start; scrubs the place en makes it ready to play again
+clean_start () {
+  echo "$dt [bb.api] Cleaning asks: Do you want me to hold still and go on? [1]"
+  echo "$dt [bb.api] Cleaning asks: Or you want to clean up the scipt deployment and quit? [2]"
+  echo "$dt [bb.api] Cleaning asks: Or do you want quit? [q]"
+  read -n1 input_cleanstart
+  if [ $input_cleanstart == 1 ]; then
+    dummy
+  else
+    if [ $input_cleanstart == 2 ]; then
+      rm -r data
+      rm apikey
+      echo "$dt [bb.api] Cleaned up te place and ill exit 1, have fun!"
+      exit 1
+    else
+      if [ $input_cleanstart == q ]; then
+        echo "$dt [bb.api] oke, byeeee...!"
+        exit 1
+      fi
+    fi
+  fi
+}
 #dummy; omdat if statements een waarde nodig hebben en ik niets wil invullen, maak ik maar een dummy functie die niks doet. Maar dan is if statement en bash wel gerustgesteld...
 dummy () {
   echo >/dev/null
@@ -69,33 +91,44 @@ fi
 #check_content; Controleren of er data beschikbaar is om mee aan de gang te gaan. Zo niet, download ik het eerst even.
 check_content () {
 if [ -f $raw_datadir/$raw_prod ]; then
-  echo "$dt [bb.api] CONTENT CHECK : "$raw_prod" is available, so not downloading nieuw content"
+  echo "$dt [bb.api] CONTENT CHECK : "$raw_prod" is available, so not downloading new content"
+  read -n1 -p "$dt [bb.api] Or do you want me to download new "$raw_prod" content? [y/*]" input_1
 else
+  if [ ! $input_1 == y ]; then
+ dummy
+  else
  get_prod
+  fi
 fi
 if [ -f $raw_datadir/$raw_img ]; then
-  echo "$dt [bb.api] CONTENT CHECK : "$raw_img" is available, so not downloading nieuw content"
+  echo "$dt [bb.api] CONTENT CHECK : "$raw_img" is available, so not downloading new content"
+  read -n1 -p "$dt [bb.api] Or do you want me to download new "$raw_img" content? [y/*]" input_2
 else
- get_prod_img
+  if [ ! $input_2 == y ]; then
+      dummy
+  else
+      get_prod_img
+  fi
 fi
 }
 #go_rince; Rincing raw data to human readable files
-go_rince () {
+go_rinse () {
 if [ $(ls $raw_prodls 2>/dev/null | wc -l) == 1 ]; then
-    echo "$dt [bb.api] DATA RINCING : found raw product data and rincing it now for human readability"
+    echo "$dt [bb.api] DATA RINSING : found raw product data and rincing it now for human readability"
     cat $raw_prodls | jq > $json_datadir/$json_prod
 fi
 if [ $(ls $raw_imagls 2>/dev/null | wc -l) == 1 ]; then
-    echo "$dt [bb.api] DATA RINCING : found raw images data and rincing it now for human readability"
+    echo "$dt [bb.api] DATA RINSING : found raw images data and rincing it now for human readability"
     cat $raw_imagls | jq > $json_datadir/$json_img
   else
-    echo "$dt [bb.api] DATA RINCING  : no raw data to rinse"
+    echo "$dt [bb.api] DATA RINSING  : no raw data to rinse"
 fi
 }
 
+clean_start
 check_auth
 check_dir
 check_content
-#go_rince
+go_rinse
 
-tree .
+tree 
