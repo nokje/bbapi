c620213d35eb346ba665b86e8faa0253d48e8327
nokje <reda-@live.nl>
2019-03-21 16:43:06 +0100

    chained

--- chain.sh
+++ chain.sh
@@ -1,5 +1,13 @@
-get_prod() {
+get_prod_id () {
   curl -s -H "Content-type: application/json" -H "Accept: application/json" -H "Authorization: Bearer NTg3ZDQ1NTZkNjQ4M2RiMDk2MGY0ODNkMzhmYzM0ZDNlZDdmNTBhN2Y2YjIzNGM5MjEzOGViMzg1Nzc1MjhlZA" https://api.sandbox.bigbuy.eu/rest/catalog/products.json?isoCode=en
 }
-
-get_prod | jq -rc '.[] | .id'
+get_prod_info () {
+    curl -s -H "Content-type: application/json" -H "Accept: application/json" -H "Authorization: Bearer NTg3ZDQ1NTZkNjQ4M2RiMDk2MGY0ODNkMzhmYzM0ZDNlZDdmNTBhN2Y2YjIzNGM5MjEzOGViMzg1Nzc1MjhlZA" https://api.sandbox.bigbuy.eu/rest/catalog/productsinformation.json?isoCode=en
+}
+live_idlist () {
+  get_prod_id | jq -rc '.[0,1,2,3,4,5,6,7,8,9,10] | .id'
+}
+get_id_info () {
+    curl -s -H "Content-type: application/json" -H "Accept: application/json" -H "Authorization: Bearer NTg3ZDQ1NTZkNjQ4M2RiMDk2MGY0ODNkMzhmYzM0ZDNlZDdmNTBhN2Y2YjIzNGM5MjEzOGViMzg1Nzc1MjhlZA" https://api.sandbox.bigbuy.eu/rest/catalog/productinformation/$id.json?isoCode=en
+}
+live_idlist | while read -r id; do get_id_info >> object_info.json ; done
